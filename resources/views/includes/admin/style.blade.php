c4275cec759891477a0e49f7e8cc4814910c5e0f
Okriiza <rendiokrizaputra@gmail.com>
2021-03-09 16:20:02 +0700

    change tag script to style

--- resources/views/includes/admin/style.blade.php
+++ resources/views/includes/admin/style.blade.php
@@ -2,7 +2,7 @@
 <link href="https://assets.okriiza.my.id/themes/backend/css/sb-admin-2.min.css" rel="stylesheet">
 <link href="https://assets.okriiza.my.id/themes/backend/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 <link href="https://assets.okriiza.my.id/themes/backend/vendor/select2/dist/css/select2.min.css" rel="stylesheet">
-<script src="https://assets.okriiza.my.id/themes/backend/vendor/ckeditor/content.css"></script>
+<link href="https://assets.okriiza.my.id/themes/backend/vendor/ckeditor/content.css" rel="stylesheet">
 <link rel="icon" href="https://assets.okriiza.my.id/themes/frontend/assets/image/logo2.png" type="image/x-icon">
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
