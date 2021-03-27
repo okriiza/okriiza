2a40c7b4e40547eb0c5202413e1edcd78cb6a41a
Okriiza <rendiokrizaputra@gmail.com>
2021-03-28 06:08:46 +0700

    change typo :P

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,15 +7,23 @@ use App\Models\Article;
 use App\Models\Category;
 use App\Models\Project;
 use Illuminate\Http\Request;
+// use Spatie\Analytics\Analytics;
+use Spatie\Analytics\Period;
+use Analytics;
 
 class DashboardController extends Controller
 {
     public function index(Request $request){
 
+        $analyticsData = Analytics::fetchVisitorsAndPageViews(Period::days(7))->all();
+        $popular = Analytics::fetchMostVisitedPages(Period::days(7), 6)->all();
+        // dd($analyticsData);
         return view('pages.admin.dashboard',[
             'artikel' => Article::count(),
             'project' => Project::count(),
             'categori' => Category::count(),
+            'analyticsData' => $analyticsData,
+            'popular' => $popular
         ]);
     }
 }
